1cf74ea021dd7a8f720c2cdda4b1a71969bd5d22
elliotghr <elliotperez879@gmail.com>
2026-08-27 12:37:57 -0600

    Modificación al Dockerfile.prod para railway

--- ms-start.sh
+++ ms-start.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-# DEFINIR LA RUTA PRINCIPAL
-export CI_PROJECT_PATH=/var/www/html
+cd /var/www/html
 
-# INSTALAR COMPOSER
-cd "${CI_PROJECT_PATH}"
-composer install --no-dev
+echo "Installing dependencies from lock file"
 
-# INSTALAR NPM
-cd "${CI_PROJECT_PATH}/public"
-npm install
+composer install --no-dev --optimize-autoloader
 
-# MODIFICAR PERMISOS DE WRITABLE
-chmod 777 -R "${CI_PROJECT_PATH}/writable"
+echo "Starting Apache"
 
-# COMPILAR EL ARCHIVO GLOBAL SASS
-sass "${CI_PROJECT_PATH}/public/css/custom-bootstrap.scss" "${CI_PROJECT_PATH}/public/css/custom-bootstrap.css"
-
-# INICIAR APACHE EN SEGUNDO PLANO
-apache2-foreground
+exec apache2-foreground
