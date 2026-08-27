e73ca772bda92b666224fc16413ac595bda1d683
elliotghr <elliotperez879@gmail.com>
2026-08-27 13:28:44 -0600

    Modificación al Dockerfile.prod para railway

--- ms-start.sh
+++ ms-start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# DEFINIR LA RUTA PRINCIPAL
-export CI_PROJECT_PATH=/var/www/html
+cd /var/www/html
 
-# INSTALAR COMPOSER
-cd "${CI_PROJECT_PATH}"
-composer install --no-dev
+echo "Installing dependencies from lock file"
+composer install --no-dev --optimize-autoloader
 
-# MODIFICAR PERMISOS DE WRITABLE
-chmod 777 -R "${CI_PROJECT_PATH}/writable"
-
-# INICIAR APACHE
-apache2-foreground
+echo "Starting Apache"
+exec apache2-foreground
